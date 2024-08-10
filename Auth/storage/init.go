1d8a63019dcaa49c9f9f747bc6eca99e9e1dca1b
Muhammad <muhammadjonxudoynazarov226@gmail.com>
2024-08-10 22:05:30 +0500

    Donw

---
+++ Auth/storage/init.go
@@ -0,0 +1,23 @@
+package storage
+
+import (
+	"auth/genproto/users"
+)
+
+type InitRoot interface {
+	Users() UsersService
+}
+
+type UsersService interface {
+	// auth
+	RegisterUser(req *users.Users) (*users.RegisterResponse, error)
+	LoginUser(req *users.LoginUserRequest) (*users.LoginResponse, error)
+	ValidateToken(req *users.ValidateTokenRequest) (*users.TokenValidationResponse, error)
+	RefreshToken(req *users.RefreshTokenRequest) (*users.TokenResponse, error)
+	ValidateEmail(req *users.VerifyEmailRequest) (*users.Empty, error)
+
+	// users
+	GetUserProfile(req *users.GetUserProfileRequest) (*users.UserProfileResponse, error)
+	UpdateUserProfile(req *users.UpdateUserProfileRequest) (*users.UserProfileResponse, error)
+	DeleteUserProfile(req *users.DeleteUserProfileRequest) (*users.Empty, error)
+}
